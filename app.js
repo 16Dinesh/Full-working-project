d56c5b8df8085cd921f68436a62264114096fa9c
test <Dineshwonks@Gmail.com>
2024-06-21 12:05:10 +0530

    name change to TreckToDo

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ main()
     .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
+    await mongoose.connect("mongodb://127.0.0.1:27017/TrekToDo")
 }
 
 app.set("view engine" , "ejs");
